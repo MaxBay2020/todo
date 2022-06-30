bd8728259871bb1ccc6ce72df032cd98e7b0306c
Max <cwang167@my.centennialcollege.ca>
2022-06-30 00:21:47 -0400

    Modified

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,7 +6,7 @@ const TodoItem = ({id, title, completed}) => {
         <article style={{margin: '10px', border: '1px solid lightgray', boxShadow: '0 2px 20px lightgray'}}>
             <div>ID: <span>{id}</span></div>
             <div>Title: <span>{title}</span></div>
-            <div>Completed: <span>{completed? '🌟' : '😵‍💫'}️</span></div>
+            <div>Completed: <span>{completed? '👏' : '😵‍💫'}️</span></div>
         </article>
     );
 };
